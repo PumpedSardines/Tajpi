00e1e76a9de6668354b39508c7edf8ff7c60f41e
Fritiof Rusck <fritiof@bjornssonrusck.se>
2022-01-28 14:05:33 +0100

    style: remove bad prints

--- Tajpi/Privlige/requestPrivilege.swift
+++ Tajpi/Privlige/requestPrivilege.swift
@@ -19,12 +19,12 @@ func pollAccessibility() {
     
     Timer.scheduledTimer(withTimeInterval: 0.3, repeats: true) { (t) in
         let hasPrivilegeNew = AXIsProcessTrusted();
-        print(hasPrivilegeNew);
+
         
         if hasPrivilegeNew && !hasPrivilege {
             hasPrivilege = true;
             let worked = startKeyboardInterception();
-            print(worked)
+ 
             delegate.rerender();
         }
         
